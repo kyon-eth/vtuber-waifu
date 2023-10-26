1f3f1113e924aa81bf22006d6857d544d67416d6
Kyon <kyonosuke.eth@gmail.com>
2023-10-26 13:03:16 +0500

    remove katakana dependency

--- utils/katakana.py
+++ utils/katakana.py
@@ -1,4 +1,4 @@
-import MeCab
+# import MeCab
 import unidic
 import pandas as pd
 import alkana
@@ -12,21 +12,21 @@ def isalpha(s):
     return alphaReg.match(s) is not None
 
 def katakana_converter(text):
-    wakati = MeCab.Tagger('-Owakati')
-    wakati_result = wakati.parse(text)
+    # wakati = MeCab.Tagger('-Owakati')
+    # wakati_result = wakati.parse(text)
 
-    df = pd.DataFrame(wakati_result.split(" "),columns=["word"])
-    df = df[df["word"].str.isalpha() == True]
-    df["english_word"] = df["word"].apply(isalpha)
-    df = df[df["english_word"] == True]
-    df["katakana"] = df["word"].apply(alkana.get_kana)
+    # df = pd.DataFrame(wakati_result.split(" "),columns=["word"])
+    # df = df[df["word"].str.isalpha() == True]
+    # df["english_word"] = df["word"].apply(isalpha)
+    # df = df[df["english_word"] == True]
+    # df["katakana"] = df["word"].apply(alkana.get_kana)
 
-    dict_rep = dict(zip(df["word"], df["katakana"]))
+    # dict_rep = dict(zip(df["word"], df["katakana"]))
 
-    for word, read in dict_rep.items():
-        try:
-            text = text.replace(word, read)
-        except:
-            pass
+    # for word, read in dict_rep.items():
+    #     try:
+    #         text = text.replace(word, read)
+    #     except:
+    #         pass
 
     return text
