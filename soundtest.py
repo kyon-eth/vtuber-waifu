fede1204e4cf9df39a01a8db764c751cc3657204
Kyon <kyonosuke.eth@gmail.com>
2023-10-27 15:37:31 +0500

    fix

--- soundtest.py
+++ soundtest.py
@@ -4,31 +4,29 @@ import wave
 import threading
 import time
 
-VIRTUAL_MIC_DEVICE_INDEX = 2  # Replace with your virtual microphone index
-
-def play_to_virtual_mic(wave_file_path):
+def play_to_virtual_mic(wave_file_path, device_index):
     
-    def stream_audio():
-        try:
-            with wave.open(wave_file_path, 'rb') as wf:
-                samplerate = wf.getframerate()
-                channels = wf.getnchannels()
+    try:
+        with wave.open(wave_file_path, 'rb') as wf:
+            samplerate = wf.getframerate()
+            channels = wf.getnchannels()
 
-                # Read all frames from the wave file
-                frames = wf.readframes(-1)
-                audio_data = np.frombuffer(frames, dtype=np.int16)
-                
-                with sd.OutputStream(device=VIRTUAL_MIC_DEVICE_INDEX, channels=channels, samplerate=samplerate, dtype='int16') as stream:
-                    stream.write(audio_data)
+            # Read all frames from the wave file
+            frames = wf.readframes(-1)
+            audio_data = np.frombuffer(frames, dtype=np.int16)
+            
+            with sd.OutputStream(device=device_index, channels=channels, samplerate=samplerate, dtype='int16') as stream:
+                stream.write(audio_data)
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    # Run the streaming in a separate thread
-    threading.Thread(target=stream_audio).start()
-    
-    
+# Define the range of device indices you want to use
+device_indices_range = range(5, 10)  # Replace with the range you prefer
+
 while True:
-    play_to_virtual_mic("output.wav")
-    time.sleep(2)
-    
+    for device_index in device_indices_range:
+        print(f"Playing sound to device index: {device_index}")
+        play_to_virtual_mic("test.wav", device_index)
+        print("...played")
+        time.sleep(2)
